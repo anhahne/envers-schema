9f6f803831d80ff108ef88898fa93d5a8de8d6bf
Andreas Hahne <andreas.hahne.84@gmail.com>
2014-02-08 09:45:14 +0100

    Implement parameter support
    Supported parameters are SQL Dialect (required), package name (required)
    and destination file (optional)

--- maven-envers-schema-plugin/src/main/java/SchemaGenerator.java
+++ maven-envers-schema-plugin/src/main/java/SchemaGenerator.java
@@ -5,27 +5,43 @@ import javax.persistence.Entity;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.envers.Audited;
 import org.hibernate.tool.EnversSchemaGenerator;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.reflections.Reflections;
 
-@Mojo(name="execute", defaultPhase=LifecyclePhase.COMPILE,
+/**
+ * Maven Plugin to generate a DDL from JPA entities which can be annotated with
+ * Hibernate Envers annotation {@link Audited}.
+ *
+ * @author Andreas Hahne
+ *
+ */
+@Mojo(name = "execute", defaultPhase = LifecyclePhase.PACKAGE,
       configurator = "include-project-dependencies",
       requiresDependencyResolution=ResolutionScope.RUNTIME)
 public class SchemaGenerator extends AbstractMojo {
-  private static final String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
-  private static final String PACKAGE = "de.dtv.volleyball.persistence";
-  private static final String FILE = "target/schema.sql";
+  private static final String DEFAULT_DESTINATION = "target/schema.sql";
 
   private Configuration config = new Configuration();
-  
+
+  @Parameter(required = true)
+  private String dialect;
+
+  @Parameter(required = true)
+  private String packageName;
+
+  @Parameter(defaultValue = DEFAULT_DESTINATION)
+  private String destination;
+
   @Override
   public void execute()
   {
-    getLog().info("Generating schema for dialect " + DIALECT);
-    this.config.setProperty("hibernate.dialect", DIALECT);
+    getLog().info("Generating schema for dialect " + this.dialect);
+    this.config.setProperty("hibernate.dialect", this.dialect);
 
     Collection<Class<?>> annotatedClasses = getAnnotatedClasses();
     getLog().info(annotatedClasses.size() + " JPA entities found.");
@@ -35,15 +51,15 @@ public class SchemaGenerator extends AbstractMojo {
     }
 
     SchemaExport export = new EnversSchemaGenerator(this.config).export();
-    export.setOutputFile(SchemaGenerator.FILE).execute(true, false, false, false);
+    export.setOutputFile(this.destination).execute(true, false, false, false);
 
-    getLog().info("Generated schema saved in " + FILE);
+    getLog().info("Generated schema saved in " + this.destination);
   }
 
   private Collection<Class<?>> getAnnotatedClasses()
   {
-    getLog().info("Searching package " + PACKAGE + " for JPA entities.");
-    Reflections reflections = new Reflections(PACKAGE);
+    getLog().info("Searching package " + this.packageName + " for JPA entities.");
+    Reflections reflections = new Reflections(this.packageName);
     return reflections.getTypesAnnotatedWith(Entity.class);
   }
 }
