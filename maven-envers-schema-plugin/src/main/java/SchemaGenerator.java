1aded0cf33ee34892f89d761cad92635306172ce
Andreas Hahne <andreas.hahne.84@gmail.com>
2014-02-08 09:14:58 +0100

    Make use of Maven logging framework

--- maven-envers-schema-plugin/src/main/java/SchemaGenerator.java
+++ maven-envers-schema-plugin/src/main/java/SchemaGenerator.java
@@ -10,40 +10,39 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.EnversSchemaGenerator;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-@Mojo(name="execute", defaultPhase=LifecyclePhase.COMPILE, configurator = "include-project-dependencies", requiresDependencyResolution=ResolutionScope.RUNTIME)
+@Mojo(name="execute", defaultPhase=LifecyclePhase.COMPILE,
+      configurator = "include-project-dependencies",
+      requiresDependencyResolution=ResolutionScope.RUNTIME)
 public class SchemaGenerator extends AbstractMojo {
   private static final String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
   private static final String PACKAGE = "de.dtv.volleyball.persistence";
   private static final String FILE = "target/schema.sql";
-  
-  private static final Logger LOG = LoggerFactory.getLogger(SchemaGenerator.class);
-  
+
   private Configuration config = new Configuration();
   
+  @Override
   public void execute()
   {
-    LOG.info("Generating schema for dialect {}.", DIALECT);
+    getLog().info("Generating schema for dialect " + DIALECT);
     this.config.setProperty("hibernate.dialect", DIALECT);
 
     Collection<Class<?>> annotatedClasses = getAnnotatedClasses();
-    LOG.info("{} JPA entities found.", annotatedClasses.size());
+    getLog().info(annotatedClasses.size() + " JPA entities found.");
     for(Class<?> c : annotatedClasses)
     {
       this.config.addAnnotatedClass(c);
     }
-    
-    SchemaExport export = new EnversSchemaGenerator(config).export();
-    export.setOutputFile(FILE).execute(true, false, false, false);
-    
-    LOG.info("Generated schema saved in {}.", FILE);
+
+    SchemaExport export = new EnversSchemaGenerator(this.config).export();
+    export.setOutputFile(SchemaGenerator.FILE).execute(true, false, false, false);
+
+    getLog().info("Generated schema saved in " + FILE);
   }
-  
+
   private Collection<Class<?>> getAnnotatedClasses()
   {
-    LOG.info("Searching package {} for JPA entities.");
+    getLog().info("Searching package " + PACKAGE + " for JPA entities.");
     Reflections reflections = new Reflections(PACKAGE);
     return reflections.getTypesAnnotatedWith(Entity.class);
   }
